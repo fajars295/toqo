1127c2edff5a287f0814c07cac7b01b8da968cf4
fajarsetiawan295 <fajarsetiawan295@gmail.com>
2020-12-15 18:26:50 +0700

    push

--- app/Http/Resources/ContendetaisResource.php
+++ app/Http/Resources/ContendetaisResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use App\Model\Content\CommentContent;
 use App\Model\Content\LikeContent;
 use App\Model\User\Profile;
+use App\User;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ContendetaisResource extends JsonResource
@@ -17,8 +18,12 @@ class ContendetaisResource extends JsonResource
      */
     public function toArray($request)
     {
+        $profile = Profile::where('users_id', $this->users_id)->first();
+
         return [
             'id' => $this->id,
+            'name' => User::find($this->users_id)->name,
+            'foto' => $profile == null ? url('logo/account.jpg') : url($profile->foto),
             'url' => url($this->foto),
             'deskripsi' => $this->deskripsi,
             'like' => LikeContent::where('contents_id', $this->id)->count(),
